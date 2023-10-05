65ce69844651799b790000f81920b2558aaee443
ibraamzoug <ibraamzoug@gmail.com>
2023-10-05 22:02:15 +0200

    added to puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *  * main - where program start to read from
+ *   * Return: 0
+ *    */
+int main(void)
+{
+			char sentence[] = "\"Programming is like building a multilingual puzzle";
+
+						puts(sentence);
+										return (0);
+}
