3f2aad75dafef92f517311bed2a76b618f3d7031
ibraamzoug <ibraamzoug@gmail.com>
2023-10-12 13:18:57 -0700

    dadd to lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,23 +1,18 @@
 #include "main.h"
 
 /**
- * print_line - print line
- * @n: input value
- *
- * Description:  draws a straight line in the terminal
- * Return: Always (0)
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character _ is printed
+ * Return: void
  */
 
 void print_line(int n)
 {
 	int i;
 
-	if (n > 0)
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			_putchar('_');
-		}
+		_putchar('_');
 	}
 	_putchar('\n');
 }
